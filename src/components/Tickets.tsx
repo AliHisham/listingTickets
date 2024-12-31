f6d3f94ae8225882bad1ab4f929e2a9e9eab8073
Ali Hesham <Ali.Hesham@itworx.com>
2024-12-31 20:39:28 +0200

    adding tailwind and displaying list of tickets

--- src/components/Tickets.tsx
+++ src/components/Tickets.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Tickets = () => {
-  return <div>Tickets</div>;
+  const url = "https://jsonplaceholder.typicode.com/comments";
+  const [tickets, setTickets] = useState<any[]>([]);
+
+  // fixed number of iterations is going to take place here in order to achieve the 10000 record number
+  const loopingHandlerForSettingData = (tickets: any[]) => {
+    for (let i = 0; i < 20; i++) {
+      setTickets((prev) => {
+        return [...prev, ...tickets];
+      });
+    }
+  };
+  const fetchingAllTickets = async () => {
+    console.log(url, "checking the url");
+    const response = await axios.get(url);
+    console.log(
+      response.data,
+      "we are just checking the response of this api "
+    );
+    loopingHandlerForSettingData(response.data);
+  };
+
+  useEffect(() => {
+    fetchingAllTickets();
+  }, []);
+  console.log(tickets, "--testing now now now ");
+  return (
+    <div>
+      {tickets &&
+        tickets.length &&
+        tickets.map((ticket, index) => {
+          return <p className="bg-green-500">{ticket.name}</p>;
+        })}
+    </div>
+  );
 };
 
 export default Tickets;
